21ee98759284ad946ce7930a51ad6ee4f2dbc860
Ruchika Verma <verma.ece.ruchika@gmail.com>
2019-07-03 05:03:16 -0400

    Update linear_classifier.py

--- Assignment1/cs231n/classifiers/linear_classifier.py
+++ Assignment1/cs231n/classifiers/linear_classifier.py
@@ -55,13 +55,10 @@ class LinearClassifier(object):
             # Hint: Use np.random.choice to generate indices. Sampling with         #
             # replacement is faster than sampling without replacement.              #
             #########################################################################
-            # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
             batch_indices = np.random.choice(num_train, batch_size, replace=False)
             X_batch = X[batch_indices]
             y_batch = y[batch_indices]
-
-            # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
             # evaluate loss and gradient
             loss, grad = self.loss(X_batch, y_batch, reg)
@@ -74,9 +71,6 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
             self.W -= learning_rate*grad
-#             pass
-
-            # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
             if verbose and it % 100 == 0:
                 print('iteration %d / %d: loss %f' % (it, num_iters, loss))
@@ -102,13 +96,9 @@ class LinearClassifier(object):
         # TODO:                                                                   #
         # Implement this method. Store the predicted labels in y_pred.            #
         ###########################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-#         pass
         scores = X.dot(self.W)
         y_pred = np.argmax(scores, axis=1)
-
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
 
     def loss(self, X_batch, y_batch, reg):
